0b03b14dc0c1cd1f6fc29f9f6049aa5b60285b09
Anish G. <anishgoyal1108@gmail.com>
2024-11-10 03:47:16 -0500

    fix: allow cors

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import json
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 
 
 def levenshtein_distance(s1, s2):
